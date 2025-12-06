92c15837a87f03f17f361a58c6f64752153d1dcc
Alceu <alceupereira3105@gmail.com>
2025-12-06 01:27:59 -0300

    Variáveis parte II

--- aula6/index.js
+++ aula6/index.js
@@ -12,3 +12,14 @@ console.log('Maria teve 1 filho com', nome, 'em 2015.');
 console.log('O filho de', nome, 'se chama Eduardo.');
 
 // let nome; // Não pode ser declarada novamente
+// Não podemos criar variáveis com palavras reservadas
+    // let let;
+    // let if;
+// Variáveis precisam ter nomes significativos (convenção)
+// Não pode começar o nome da variável com um número
+// Não podem conter espaços ou traços
+// Utilize camelCase
+    // let nomeCompletoDoCliente = 'Luiz Otávio'';
+// Variáveis são Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize VAR, utilize LET
