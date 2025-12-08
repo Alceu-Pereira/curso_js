09b8805f260930699e6d722ac801dcc564bb5d30
Alceu <alceupereira3105@gmail.com>
2025-12-07 22:31:39 -0300

    Alert, prompt e confirm

---
+++ aula11/js/script.js
@@ -0,0 +1,4 @@
+const num1 = prompt('Digite um número: ');
+const num2 = prompt('Digite outro número: ');
+const resultado = parseFloat(num1) + parseFloat(num2);
+alert(`O resultado da operação foi: ${resultado}`);
