3e2028e242661cbfeaf678952f6d3119c962db1e
Alceu <alceupereira3105@gmail.com>
2025-12-11 00:23:02 -0300

    Exercicio const e let

---
+++ aula8/index.js
@@ -0,0 +1,9 @@
+// Nome tem X anos, pesa X kg tem X de altura e seu IMC é de X
+
+let nome = 'Alceu Augusto Pereira de Freitas';
+let idade = 25;
+let peso = 75;
+let altura = 1.65;
+let imc = peso / altura**2 ;
+// console.log(nome, 'tem', idade, 'anos, pesa', peso, 'kg tem', altura, 'e seu IMC é de', imc);
+console.log(`{${nome} tem ${idade} anos, pesa ${peso} kg tem ${altura}m e seu IMC é de ${imc}`);
