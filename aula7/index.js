d1769fd53de499d2593883a23a754ee47ecaaeab
Alceu <alceupereira3105@gmail.com>
2025-12-11 00:17:06 -0300

    Constantes em JS

---
+++ aula7/index.js
@@ -0,0 +1,8 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultadoDuplicado = resultado * 2;
+let resultadoTriplicado = resultadoDuplicado * 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+console.log(resultadoTriplicado);
+console.log(typeof resultadoTriplicado);
