59836874cdd500628651ca93f79311eb11263e0f
Alceu <alceupereira3105@gmail.com>
2025-12-11 23:24:06 -0300

    Operadores aritméticos, incremento/decremento e operadores de atribuição

---
+++ aula10/index.js
@@ -0,0 +1,45 @@
+/**
+ * Aritméticos
+ * + Adição / Concatenação 
+ * - Subtração
+ * / Divisão
+ * * Multiplicação
+ * ** Potenciação
+ * % Resto da divisão
+**/
+
+// const num1 = 10;
+// const num2 = 5;
+// const num3 = 10;
+// console.log((num1 + num2) * num3);
+
+// let contador = 1;
+// ++contador; // 1
+// ++contador; // 2
+// ++contador; // 3
+// ++contador; // 4
+// console.log(contador);
+
+// let contador = 1;
+// console.log(contador++);
+// console.log(contador);
+
+
+// const passo = 2;
+// let contador = 0;
+// contador += passo;
+// console.log(contador);
+// contador += passo;
+// console.log(contador);
+// contador += passo;
+// console.log(contador);
+// contador += passo;
+// console.log(contador);
+
+// NaN - Not a number
+const num1 = '2.5';
+const num2 = '12';
+const num3 = Number(num1);
+const num4 = parseInt(num2);
+console.log(Number(num3 * num4));
+console.log(typeof num3);
