bc26ac86e1b81505adf493068c76e9c066f8a7b0
Alceu <alceupereira3105@gmail.com>
2025-12-14 03:55:20 -0300

    Metodos com strings

---
+++ aula13/index.js
@@ -0,0 +1,26 @@
+// let umaString = "Um texto";
+// let umaString = "Um \rr \"texto\"";
+
+//               01234567   
+// let umaString = "Um texto";
+// console.log(umaString.charAt(6));
+// console.log(umaString.concat('a', 'b', 'c'));
+// console.log(umaString + 'a' + 'b' + 'c');
+// console.log(`${umaString}abc`);
+// console.log(umaString.indexOf('t'));
+// console.log(umaString.indexOf('9'));
+// console.log(umaString.indexOf('t', 4));
+// console.log(umaString.match(/[a-z]/g));
+// console.log(umaString.search(/[x]/));
+// console.log(umaString.replace('Um', 'Outra'));
+
+//               0123456789...
+let umaString = "O rato roeu a roupa do rei de roma.";
+
+// console.log(umaString.replace(/r/g, '#'));
+// console.log(umaString.length)
+// console.log(umaString.slice(2, 6));
+// console.log(umaString.substring(0, 5));
+// console.log(umaString.split(' '));
+console.log(umaString.toUpperCase());
+console.log(umaString.toLowerCase());
