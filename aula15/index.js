68da06094b93a2a71f47d6580d57006f41ce17c4
Alceu Pereira <alceupereira3105@gmail.com>
2025-12-21 02:58:14 -0300

    Estudo sobre Math

--- aula15/index.js
+++ aula15/index.js
@@ -4,6 +4,6 @@ let num1 = 9.49;
 // let num2 = Math.round(num1);
 // let num2 = Math.max(1, 2, 3, 5, 5.88);
 // let num2 = Math.min(1, 2, 3, 5, 5.88);
-const aleatorio = Math.random();
-
-console.log(aleatorio);
+// const aleatorio = Math.random();
+// console.log(Math.pow(2, 10));
+console.log(!!(100/0));
