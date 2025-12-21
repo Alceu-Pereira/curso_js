33a095e47b60267d1de2787226f9e5ac2f881c36
Alceu Pereira <alceupereira3105@gmail.com>
2025-12-21 01:19:10 -0300

    Objeto MATH

---
+++ aula15/index.js
@@ -0,0 +1,9 @@
+let num1 = 9.49;
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+// let num2 = Math.max(1, 2, 3, 5, 5.88);
+// let num2 = Math.min(1, 2, 3, 5, 5.88);
+const aleatorio = Math.random();
+
+console.log(aleatorio);
